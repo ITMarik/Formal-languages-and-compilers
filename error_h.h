7f417c057d4c879812aa0d99eae588bba4bc3618
ITMarik <44090779+ITMarik@users.noreply.github.com>
2018-11-02 18:34:39 +0100

    Add files via upload

--- error_h.h
+++ error_h.h
@@ -4,14 +4,14 @@
 #ifndef error_h
 #define error_h
 
-#define SUCCES 0
-#define LEX_ERROR 1
-#define SYNT_ERROR 2
-#define SEMANTIC_ERROR 3
-#define RUNTIME_ERROR 4
-#define PARAM_ERROR 5
-#define OTHER_ERROR 6
-#define ZERO_DIVISION_ERROR 9
-#define PARSER_ERROR 99
+#define SUCCES 0		//everything was OK
+#define LEX_ERROR 1		//there is an error in lexical analysis
+#define SYNT_ERROR 2	//there is an error in syntactically analysis
+#define SEMANTIC_ERROR 3 //there is an error with undefined function or undefined variable
+#define RUNTIME_ERROR 4	//there is an error in arithmetical, string or relation expresions
+#define PARAM_ERROR 5	//wrong number of parameters
+#define OTHER_ERROR 6 	//other semantic mistake
+#define ZERO_DIVISION_ERROR 9	//error in division with 0
+#define PARSER_ERROR 99		//error in parser unaffected by input program
 
 #endif
