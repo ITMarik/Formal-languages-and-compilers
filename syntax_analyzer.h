c7e417465c137ef63542d45fa740f7794892a8c9
Rogulus <37539467+Rogulus@users.noreply.github.com>
2018-12-01 16:14:13 +0100

    Update syntax_analyzer.h

--- syntax_analyzer.h
+++ syntax_analyzer.h
@@ -15,27 +15,28 @@ typedef enum token_type
 	T_IF,
 	T_WHILE,
 	T_DO,
-	T_ELSE,
+	T_ELSE,	// 5
 	T_END,
 	T_THEN,
 	T_EOL,
 	T_EOF,
-	T_COMMA,
+	T_COMMA, // 10
 	T_STRING,
 	T_INT,
 	T_FLOAT,
 	T_L_BRACKET,
-	T_R_BRACKET,
+	T_R_BRACKET, // 15
 	T_PLUS,
 	T_MINUS,
 	T_MULTIPLICATION,
 	T_DIVISION,
-	T_EQUAL,
+	T_EQUAL, //20
 	T_NOT_EQUAL,
 	T_LESS,
 	T_GREATER,
 	T_LESS_EQUAL,
 	T_GREATER_EQUAL,
+	T_NIL
 } TOKEN_TYPE;
 
 
@@ -49,7 +50,7 @@ typedef struct token
 
 void next_token(int *ToKeN_OrDeR, t_token *P_token);
 
-
+bool assigned(int * ToKeN_OrDeR, t_token *P_token);
 bool f_call(int *ToKeN_OrDeR, t_token * P_token);
 bool stat_list(int *ToKeN_OrDeR, t_token * P_token);
 
@@ -67,7 +68,7 @@ void next_token(int *ToKeN_OrDeR, t_token *P_token)
 	switch(*ToKeN_OrDeR)
 	{
 		case 1:
-		P_token->type = T_WHILE;
+		P_token->type = T_DEF;
 		P_token->p_token_attributes = NULL;
 		break; 
 		
@@ -77,35 +78,75 @@ void next_token(int *ToKeN_OrDeR, t_token *P_token)
 		break; 
 	
 		case 3:
-		P_token->type = T_DO;
+		P_token->type = T_L_BRACKET;
 		P_token->p_token_attributes = NULL;
 		break; 
 		
 		case 4:
-		P_token->type = T_EOL;
+		P_token->type = T_ID;
 		P_token->p_token_attributes = NULL;
 		break; 
 		
 		case 5:
-		P_token->type = T_EOL;
+		P_token->type = T_COMMA;
 		P_token->p_token_attributes = NULL;
 		break; 
 		
 		case 6:
-		P_token->type = T_END;
+		P_token->type = T_ID;
 		P_token->p_token_attributes = NULL;
 		break;
 		
 		case 7:
-		P_token->type = T_EOL;
+		P_token->type = T_R_BRACKET;
 		P_token->p_token_attributes = NULL;
 		break; 
 		
 		case 8:
-		P_token->type = T_EOF;
+		P_token->type = T_EOL;
 		P_token->p_token_attributes = NULL;
 		break;
 		
+		case 9:
+		P_token->type = T_ID;
+		P_token->p_token_attributes = NULL;
+		break; 
+		
+		case 10:
+		P_token->type = T_EQUAL;
+		P_token->p_token_attributes = NULL;
+		break; 
+	
+		case 11:
+		P_token->type = T_INT;
+		P_token->p_token_attributes = NULL;
+		break; 
+		
+		case 12:
+		P_token->type = T_EOL;
+		P_token->p_token_attributes = NULL;
+		break; 
+		
+		case 13:
+		P_token->type = T_END;
+		P_token->p_token_attributes = NULL;
+		break; 
+		
+		case 14:
+		P_token->type = T_EOL;
+		P_token->p_token_attributes = NULL;
+		break;
+		
+		case 15:
+		P_token->type = T_EOF;
+		P_token->p_token_attributes = NULL;
+		break; 
+		
+		case 16:
+		P_token->type = T_EOF;
+		P_token->p_token_attributes = NULL;
+		break;
+
 		default:
 		P_token->type = T_COMMA; 
 	} 
