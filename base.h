0424465069042243329dcbb0af075fcb2b29315c
ITMarik <44090779+ITMarik@users.noreply.github.com>
2018-12-02 15:17:53 +0100

    Update base.h

--- base.h
+++ base.h
@@ -3,7 +3,7 @@
 
 int BinaryToDecimal(long long n); //function to convert binary number to decimal number
 int hexadecimalToDecimal(char hexVal[]); //function to convert hexadecimal number to decimal number
-int DecimalToOctal(int decimalNumber); //function to convert octal number to decimal number
+long long convertOctalToDecimal(int octalNumber); //function to convert octal number to decimal number
 
 
 #endif //BASE_H
