73e57a773a2fdc3bf7e95eb5e243cd3b6198431a
ITMarik <44090779+ITMarik@users.noreply.github.com>
2018-12-04 17:41:13 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#include "syntax_analyzer.h"
+#include "err.h"
+
+FILE *fp;
+
+int main(int argc, char const *argv[])
+{
+
+
+	if (argc == 1) {
+		vypis_hodnotu(5);
+	}
+
+	
+	return 0;
+}
