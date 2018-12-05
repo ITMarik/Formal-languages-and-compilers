58d087c37426ee2c881227a78fb8c249a5c3dc62
ITMarik <44090779+ITMarik@users.noreply.github.com>
2018-12-05 21:56:36 +0100

    Update err.c

--- err.c
+++ err.c
@@ -3,7 +3,7 @@
  * Soubor :        err.c - funkce pro vypis erroru           		*
  * Datum :         5.12. 2018                                           *
  * Projekt :       Implementace překladače imperativního jazyka IFJ 18  *
- * Členové týmu :  Martin Janda                                         *                                 *
+ * Autoři :  	   Martin Janda                                         *                                 
  * Varianta :      Tým 123, varianta I                                  *
  ***********************************************************************/
 
