db6129b9e0c38dd64763235393b25b8a7bf9c59f
ITMarik <44090779+ITMarik@users.noreply.github.com>
2018-12-05 21:54:36 +0100

    Update generator.c

--- generator.c
+++ generator.c
@@ -3,10 +3,7 @@
  * Soubor :        generator.c - vlastní generování, 3 adresný kód      *
  * Datum :         5.12. 2018                                           *
  * Projekt :       Implementace překladače imperativního jazyka IFJ 18  *
- * Členové týmu :  Martin Janda                                         *
- *              :  Marek Šťastný                                        *
- *              :  Martina Tučková                                      *
- *              :  Martina Jendrálová                                   *
+ * Autoři :        Martin Janda                                         *                                  *
  * Varianta :      Tým 123, varianta I                                  *
  ***********************************************************************/
 
